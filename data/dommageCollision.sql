1951bde79c449cbc8b49e3f19cd735c3b599f703
abdeliaassou <abdeliaassou@gmail.com>
2019-12-19 21:40:14 +0100

    Remove data breach

--- data/dommageCollision.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO	t_dommagecollision (id, codeCompagnie, codeUsage, codeClasse, codeSousClasse, carburant, puissanceFiscale,
-formuleCollision, primeFixe, franchise, plafond, tauxCommission, tauxTPS, tauxTaxe, observation)
-VALUES
-(1,1,"A","D12",1,"D",9,1,581.00,0.00,100000.00,0.00,0.00,14.00,""),
-(2,1,"A","D12",2,"D",9,1,929.60,5.00,2500.00,12.00,12.28,0.00,"");
